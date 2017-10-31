e50575ec0b1a3b8fe51fb07aa6d20cc5fe951670
s0556150 <d.b.extras@gmx.de>
2017-10-31 15:50:50 +0100

    added index.html, herotable.html, vita.html 3x, contact.html, impressum.html, faq.html, style.css, deploy.sh

---
+++ deploy_staticSite.sh
@@ -0,0 +1,13 @@
+#!bin/bash
+scp index.html s0556150@remserv.rz.htw-berlin.de:public_html
+scp herotable.html s0556150@remserv.rz.htw-berlin.de:public_html
+scp contact.html s0556150@remserv.rz.htw-berlin.de:public_html
+scp faq.html s0556150@remserv.rz.htw-berlin.de:public_html
+scp impressum.html s0556150@remserv.rz.htw-berlin.de:public_html
+scp vita_batgirl.html s0556150@remserv.rz.htw-berlin.de:public_html
+scp vita_batman.html s0556150@remserv.rz.htw-berlin.de:public_html
+scp vita_cpt_america.html s0556150@remserv.rz.htw-berlin.de:public_html
+scp email.html s0556150@remserv.rz.htw-berlin.de:public_html
+scp style.css s0556150@remserv.rz.htw-berlin.de:public_html
+
+curl home.htw-berlin.de/~s0556150
